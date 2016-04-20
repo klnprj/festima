8d50ef4dca4b11e0fe3b2db80c8d31120a2995a6
klnprj <klinkerproject@gmail.com>
2016-04-20 17:10:36 +0300

    initial 2gis maps

--- src/app/building/building.show.controller.js
+++ src/app/building/building.show.controller.js
@@ -9,5 +9,49 @@ angular.module('festima')
       function(building) {
         vm.building = building;
       }
-    )
-  });
+    );
+
+    DG.then(function () {
+      var map, point, lat, lng, marker;
+
+      map = DG.map('map', {
+        center: [54.9802, 82.8980],
+        zoom: 18
+      });
+
+      DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
+
+      DG.ajax({
+        url: 'http://catalog.api.2gis.ru/geo/search',
+        data: {
+          key: 'ruczoy1743',
+          version: 1.3,
+          q: 'Москва, Красная площадь, 2'
+        },
+        type: 'GET',
+        success: function(data) {
+          if (typeof marker !== 'undefined') {
+            map.removeLayer(marker);
+          }
+
+          // считываем строку в WKT-формате и возвращаем объект Point
+          point = DG.Wkt.toPoints(data.result[0].centroid);
+
+          // извлекаем координаты для маркера
+          lng = point[0];
+          lat = point[1];
+
+          // создаем и добавляем маркер на карту
+          marker = DG.marker([lat, lng]);
+          map.addLayer(marker);
+
+          // центрируем карту в координаты маркера
+          map.panTo([lat, lng]);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    });
+  }
+);
