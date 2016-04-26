7fb1e83c097e77ddec6195d0efe08f45fa5ad9a7
klnprj <klinkerproject@gmail.com>
2016-04-26 15:15:49 +0300

    added maps service

--- src/app/building/building.create.controller.js
+++ src/app/building/building.create.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('festima')
-  .controller('BuildingCreateController', function($location, Building, toastr) {
+  .controller('BuildingCreateController', function($location, Building, maps, toastr) {
     var vm = this;
 
     vm.building = new Building();
@@ -15,50 +15,46 @@ angular.module('festima')
 
       toastr.success('Новый объект "' + vm.building.name + '" добавлен!');
     };
-    
-    vm.searchAddress = function(q) {
-      DG.then(function () {
-        var map, point, lat, lng, marker;
 
-        map = DG.map('map', {
-          center: [54.9802, 82.8980],
-          zoom: 18
-        });
+    maps.initMap().then(function(map) {
+      vm.map = map;
 
-        DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
+      vm.map.on('click', function (e) {
+        var latLng = [e.latlng.lat, e.latlng.lng];
+        console.log(latLng);
 
-        DG.ajax({
-          url: 'http://catalog.api.2gis.ru/geo/search',
-          data: {
-            key: 'ruczoy1743',
-            version: 1.3,
-            q: q
-          },
-          type: 'GET',
-          success: function(data) {
-            if (typeof marker !== 'undefined') {
-              map.removeLayer(marker);
-            }
+        maps.searchCoords(latLng).then(function (location) {
+          vm.location = location;
+          vm.address = location.name;
 
-            // считываем строку в WKT-формате и возвращаем объект Point
-            point = DG.Wkt.toPoints(data.result[0].centroid);
-
-            // извлекаем координаты для маркера
-            lng = point[0];
-            lat = point[1];
-
-            // создаем и добавляем маркер на карту
-            marker = DG.marker([lat, lng]);
-            map.addLayer(marker);
-
-            // центрируем карту в координаты маркера
-            map.panTo([lat, lng]);
-          },
-          error: function(error) {
-            console.log(error);
+          if (typeof vm.marker !== 'undefined') {
+            vm.map.removeLayer(vm.marker);
           }
+
+          vm.marker = maps.getMarker(latLng);
+          var text = location.name + '.<br />';
+
+          text += location.attributes.buildingname;
+          vm.marker.bindPopup(text);
+          vm.marker.addTo(vm.map);
         });
-      });      
-    };   
+      });
+    });
+
+    vm.searchAddress = function(q) {
+      maps.searchAddress(q).then(function(latLng){
+
+        if (typeof vm.marker !== 'undefined') {
+          vm.map.removeLayer(vm.marker);
+        }
+
+        // создаем и добавляем маркер на карту
+        vm.marker = maps.getMarker(latLng);
+        vm.map.addLayer(vm.marker);
+
+        // центрируем карту в координаты маркера
+        vm.map.panTo(latLng);
+      });
+    };
   }
 );
---
+++ src/app/building/map/map.service.js
@@ -0,0 +1,94 @@
+'use strict';
+
+(function() {
+  angular.module('festima')
+    .service('maps', function($q) {
+      var dgis;
+
+      angular.extend(this, {
+        dgis: dgis,
+
+        initMap: function () {
+          var deferred = $q.defer();
+
+          DG.then(function () {
+            dgis = DG;
+            var map = dgis.map('map', {
+              center: [54.981, 82.891],
+              zoom: 16,
+              geoclicker: true,
+              showPhotos: false,
+              showBooklet: false
+            });
+
+            dgis.marker([54.98, 82.89]).addTo(map).bindPopup('Clicked!');
+
+            deferred.resolve(map);
+          }, function() {
+            deferred.reject();
+          });
+
+          return deferred.promise;
+        },
+
+        searchAddress: function (q) {
+          var point, lng, lat;
+          var deferred = $q.defer();
+
+          dgis.ajax({
+            url: 'http://catalog.api.2gis.ru/geo/search',
+            data: {
+              key: 'ruczoy1743',
+              version: 1.3,
+              q: q
+            },
+            type: 'GET',
+            success: function (data) {
+              // считываем строку в WKT-формате и возвращаем объект Point
+              point = dgis.Wkt.toPoints(data.result[0].centroid);
+
+              // извлекаем координаты для маркера
+              lng = point[0];
+              lat = point[1];
+
+              deferred.resolve([lat, lng]);
+            },
+            error: function (error) {
+              console.log(error);
+              deferred.reject();
+            }
+          });
+
+          return deferred.promise;
+        },
+
+        searchCoords: function (latLng) {
+          var deferred = $q.defer();
+
+          dgis.ajax({
+            url: 'http://catalog.api.2gis.ru/geo/search',
+            data: {
+              key: 'ruczoy1743',
+              version: 1.3,
+              q: latLng[1] + ',' + latLng[0]
+            },
+            success: function (data) {
+              
+              deferred.resolve(data.result[0]);
+            },
+            error: function (error) {
+              console.log(error);
+              deferred.reject();
+            }
+          });
+
+          return deferred.promise;
+        },
+
+        getMarker: function(latLng) {
+          return dgis.marker(latLng);
+        }
+      });
+    }
+  );
+})();
