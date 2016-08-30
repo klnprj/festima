a76e37446ec135f8f60db2ed7968ef60c35c391e
klnprj <klinkerproject@gmail.com>
2016-08-30 21:26:04 +0300

    dictionaries with mock backend

--- src/app/components/dictionary/dictionary.component.js
+++ src/app/components/dictionary/dictionary.component.js
@@ -12,6 +12,25 @@
   function DictionaryComponentController(dictionaries) {
     var vm = this;
 
+    vm.items = [];
+    vm.newItem = {};
+    vm.currentPage = 1;
+    vm.totalItems = vm.items.length;
+    vm.itemsPerPage = 1;
+
+    vm.add = function(item) {
+      dictionaries.addItem(vm.dictionary.key, item);
+      vm.newItem = {};
+    };
+
+    vm.pageChanged = function() {
+      dictionaries.itemsByKey(vm.dictionary.key, (vm.currentPage - 1) * vm.itemsPerPage, vm.itemsPerPage).then(function(items) {
+        vm.items = items;
+      });
+      console.log('Page changed to: ', vm.currentPage);
+    };
+
+    vm.pageChanged();
     console.log('Dictionary: ', vm.dictionary);
   }
 
--- src/app/dictionary/dictionary.service.js
+++ src/app/dictionary/dictionary.service.js
@@ -3,13 +3,25 @@
 
   app.service('dictionaries', function($q, $http) {
 
+    var dictionaries = {
+      customers: {title: 'Заказчики', key: 'customers', items: [{name: 'uno'}, {name: 'dos'}]}
+    };
+
     angular.extend(this, {
       listAll: function() {
         return $q.when([{title: 'Заказчики', key: 'customers'}, {title: 'Проектные организации', key: 'designers'}]);
       },
 
       loadByKey: function(key) {
-        return $q.when({title: 'Заказчики', key: 'customers', items: [{name: 'uno'}, {name: 'dos'}]});
+        return $q.when(dictionaries[key]);
+      },
+
+      itemsByKey: function(key, offset, max) {
+        return $q.when(dictionaries[key].items.slice(offset, offset + max));
+      },
+
+      addItem: function(key, item) {
+        return $q.when(dictionaries[key].items.push(item));
       }
     });
   });
