30a6a50417dafbcc655533e7823f1ffba5157533
klnprj <klinkerproject@gmail.com>
2016-10-19 22:59:48 +0300

    fix улучшение интерфейса управления позициями

--- src/app/components/dealerPositions/dealerPositions.component.js
+++ src/app/components/dealerPositions/dealerPositions.component.js
@@ -1,12 +1,50 @@
-angular.module('festima').component('esDealerPositions', {
-  templateUrl: 'app/components/dealerPositions/dealer-positions.html',
-  bindings: {
-    positions: '<',
-    newPosition: '<',
-    onAddPosition: '&',
-    onDeletePosition: '&'
-  },
-  controller: 'DealerPositionsController',
-  controllerAs: 'positionsVm',
-  transclude: true
-});
+(function(app) {
+  'use strict';
+
+  app.component('esDealerPositions', {
+    templateUrl: 'app/components/dealerPositions/dealer-positions.html',
+    bindings: {
+      positions: '<',
+      newPosition: '<',
+      onAddPosition: '&',
+      onDeletePosition: '&'
+    },
+    controller: DealerPositionsController,
+    controllerAs: 'positionsVm',
+    transclude: true
+  });
+
+  /** @ngInject */
+  function DealerPositionsController() {
+    var vm = this;
+
+    vm.types = ['фасады', 'интерьеры'];
+    vm.statuses = ['на согласовании', 'утвержден', 'отгружен','не согласован'];
+    // vm.statuses = [
+    //   { code: 'PRESALE', label: 'на согласовании' },
+    //   { code: 'APPROVED', label: 'утвержден' },
+    //   { code: 'SHIPPED', label: 'отгружен' },
+    //   { code: 'REJECTED', label: 'не согласован' }
+    // ];
+
+    angular.extend(vm, {
+
+      addNewPosition: function() {
+        var newPosition = angular.copy(vm.newPosition);
+        angular.extend(newPosition, vm.inputPosition);
+        vm.inputPosition = {};
+        vm.onAddPosition({dealerId: newPosition.dealerId, position: newPosition});
+      },
+
+      removePosition: function(position) {
+        vm.onDeletePosition({dealerId: position.dealer.id, position: position});
+      },
+
+      activePosition: function(item) {
+        return item.removed === undefined;
+      }
+
+    });
+  }
+
+}(angular.module('festima')));
