afa240180118f5c7148301526f42dfb89ef325be
klnprj <klinkerproject@gmail.com>
2016-10-20 13:11:41 +0300

    закомментирована обработка ошибок не найденных объектов

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -5,11 +5,11 @@
     .module('festima')
     .run(runBlock)
     .run(['$rootScope', '$window', '$state', 'OAuth', 'toastr', function($rootScope, $window, $state, OAuth, toastr) {
-      $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-        event.preventDefault();
-        toastr.error('Объект не найден');
-        $state.go('private.buildinglist');
-      });
+      // $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+      //   event.preventDefault();
+      //   toastr.error('Объект не найден');
+      //   $state.go('private.buildinglist');
+      // });
 
       $rootScope.$on('oauth:error', function(event, rejection) {
         // Ignore `invalid_grant` error - should be catched on `LoginController`.
